80529db344d0eeb8a7dd61a8002a1e56fe2c9e71
zubayer-talukder <121654019+zubayer-talukder@users.noreply.github.com>
2023-02-24 15:45:32 +0600

    Add files via upload

---
+++ 1.Write a program to print your name, date of birth, and mobile number/1.  Name , DOB, Mobile number  display.c
@@ -0,0 +1,10 @@
+//problem no 1.	Write a program to print your name, date of birth, and mobile number.
+#include<stdio.h>
+int main ()
+{
+
+    printf("Name: Abdullah Zubayer Talukder\n");
+    printf("Date of birth: 1 Jan 1900\n");
+    printf("Mobile Number: +88015XXXXXXXX");
+    return 0;
+}
