88cf37f358a6392f3ad756430a436a7add6d1e1f
zubayer-talukder <121654019+zubayer-talukder@users.noreply.github.com>
2023-03-02 00:40:15 +0600

    Add files via upload

---
+++ 5.area of cicle/5. Area of circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float r, area;
+    printf("Give me your ridus of cicle :\n");
+    scanf("%f",&r);
+    area=3.1415*r*r;
+    printf("\n Your result is: %f",area);
+ return 0;
+}
