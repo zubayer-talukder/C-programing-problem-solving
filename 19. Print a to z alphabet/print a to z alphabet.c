1647460f2292c89057003eb796f4a97ab5ff81b3
Abdullah Zubayer Talukder <121654019+zubayer-talukder@users.noreply.github.com>
2023-03-09 02:07:05 +0600

    Add files via upload

---
+++ 19. Print a to z alphabet/print a to z alphabet.c
@@ -0,0 +1,14 @@
+// Write a code that prints a to z letter
+#include<stdio.h>
+int main()
+{
+
+    char ch;
+    printf("This program Will print A to Z:\n");
+    for(ch='a';ch<='z';ch++)
+    {
+        printf("%c  ",ch);
+    }
+getch();
+}
+
