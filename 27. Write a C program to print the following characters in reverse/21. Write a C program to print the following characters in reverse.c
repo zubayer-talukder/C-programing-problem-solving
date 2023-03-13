33149ecc6480f8dceabe99908ad4d6b7a0c6bc40
Abdullah Zubayer Talukder <121654019+zubayer-talukder@users.noreply.github.com>
2023-03-13 19:26:10 +0600

    Add files via upload

---
+++ 27. Write a C program to print the following characters in reverse/21. Write a C program to print the following characters in reverse.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+  char ch1,ch2,ch3;
+  printf("Enter three alphabets:");
+  scanf("%c%c%c",&ch1,&ch2,&ch3);
+ printf("The alphabets in reverse order are:%c,%c,%c",ch3,ch2,ch1);
+getch();
+}
