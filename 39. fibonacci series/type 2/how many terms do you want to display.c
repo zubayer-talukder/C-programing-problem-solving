74e639b020598ab034f81b71c49360dbac197ec3
Abdullah Zubayer Talukder <121654019+zubayer-talukder@users.noreply.github.com>
2023-03-22 01:52:07 +0600

    Add files via upload

---
+++ 39. fibonacci series/type 2/how many terms do you want to display.c
@@ -0,0 +1,21 @@
+// fibonacci series problem : send me the highest value .
+#include<stdio.h>
+int main()
+{
+    int first=0,second=1,fibo,n,i;
+    printf("Send me the highest positive value!!:");
+    scanf("%d",&n);
+
+    fibo=first+second;
+    printf("Fibonnaccci series:%d,%d,",first,second);
+    while(fibo<=n)
+    {
+        printf("%d,",fibo);
+        fibo=first+second;
+
+        first=second;
+        second=fibo;
+    }
+getch();
+
+}
