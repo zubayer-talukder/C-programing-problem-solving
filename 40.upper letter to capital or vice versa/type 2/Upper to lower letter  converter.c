ffeeedb01a1afc51c6fe9ddc2fe9ac81592989f8
Abdullah Zubayer Talukder <121654019+zubayer-talukder@users.noreply.github.com>
2023-03-22 02:07:51 +0600

    Add files via upload

---
+++ 40.upper letter to capital or vice versa/type 2/Upper to lower letter  converter.c
@@ -0,0 +1,14 @@
+//lower letter to upper letter conversion
+#include<stdio.h>
+int main()
+{
+
+        char ch;
+
+        printf("Give me Lower letter:");
+        scanf("%c",&ch);
+        ch=ch+32;
+        printf("\n Your Upper letter is :%c",ch);
+        getch();
+
+}
