5b62bf3910b7020061446ed6e97e82884197588d
Abdullah Zubayer Talukder <121654019+zubayer-talukder@users.noreply.github.com>
2023-04-20 03:42:12 +0600

    Add files via upload

---
+++ 57. print ASCII value of a character/print ASCII value of a character.c
@@ -0,0 +1,9 @@
+// write a program that will print ASCII value of a character
+#include<stdio.h>
+int main(){
+    char ch;
+    printf(" Give me any Character: ");
+    scanf("%c",&ch);
+    printf("You Charater's ASCII value is : %d ",ch);
+
+}
