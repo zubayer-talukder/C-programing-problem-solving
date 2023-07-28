ecfdd05b137535df7ccac0e753f237d805bcf571
Abdullah Zubayer Talukder <121654019+zubayer-talukder@users.noreply.github.com>
2023-07-28 06:21:05 +0600

    Add files via upload

---
+++ 59. Decimal to octal and hexadecimal converter/Decimal_to_octal_and_Hexadecimal_converter.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int x;
+    printf("Enter a Number : ");
+    scanf("%d",&x);
+    // %i can be integar type data casting
+    printf("Octal Number : %o \n",x);
+    // %o stands for octal number 
+    printf("Hexadecimal Number : %x \n",x);
+    // %x stands for Hexadecimal number
+}
