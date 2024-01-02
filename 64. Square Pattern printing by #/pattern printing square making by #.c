0c4a05b72cf1e123335a3f905c5b41c6a4f0b891
Abdullah Zubayer Talukder <121654019+zubayer-talukder@users.noreply.github.com>
2024-01-03 02:11:31 +0600

    suessfull

---
+++ 64. Square Pattern printing by #/pattern printing square making by #.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int n;
+printf("Give me a number: ");
+scanf("%d",&n);
+for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        printf("# ");
+    }
+    printf("\n");
+
+}
+return 0;
+}
